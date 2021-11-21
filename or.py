cf7f14c363deaaf1d88e673b72d41f367a167d1a
Jaiyesh Chahar <jaiyesh0002@gmail.com>
2021-11-22 00:44:05 +0530

    code organised

--- or.py
+++ or.py
@@ -1,8 +1,8 @@
-from matplotlib.pyplot import plot
 from utils.model import Perceptron
 from utils.all_utils import prepare_data, save_model, save_plot
 import pandas as pd 
 import numpy as np
+
 
 
 def main(data,eta,epochs,filename,plotfilename):
@@ -21,14 +21,13 @@ def main(data,eta,epochs,filename,plotfilename):
 
 
 if __name__ =='__main__':   ## Entry point for this file
-    AND = {
+    OR = {
         'x1':[0,0,1,1],
         'x2': [0,1,0,1],
-        'y': [0,0,0,1]
+        'y': [0,1,1,1]
 
     }
     ETA = 0.3 #Between 0 and 1
     EPOCHS = 10
 
-    main(data = AND,eta=ETA,epochs=EPOCHS,filename='and.model',plotfilename='and.png')     ### Start execution from here
-        
+    main(data = OR,eta=ETA,epochs=EPOCHS,filename='or.model',plotfilename='or.png')     ### Start execution from here
